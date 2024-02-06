49654e3d421d33f039c72f94daf345e6addb012a
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2024-02-06 18:27:05 +0100

    Add package manifest and Runtime asmdef

---
+++ Runtime/AdvancedButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexH.AdvancedGUI{
+    public class AdvancedButton : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
