8ca7fa08aff76ad071d58cd4cfd133c3f7105b66
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2024-02-09 21:29:26 +0100

    Add InputSystem dependency and add MouseParallax

---
+++ Runtime/MouseParallax.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AlexH.AdvancedGUI
+{
+    public class MouseParallax : MonoBehaviour
+    {
+        private Vector3 _screenMiddle;
+        private Resolution _currentResolution;
+        private Mouse _currentMouse;
+
+        private RectTransform _rectTransform;
+        private Vector3 _defaultPosition;
+
+        [Range(0, 1)]
+        [SerializeField] private float parallaxStrength = 0.25f;
+
+        private void Start()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _defaultPosition = _rectTransform.localPosition;
+        }
+        
+        private void Update()
+        {
+            _rectTransform.localPosition = _defaultPosition - (ScreenCenterToMouse()*parallaxStrength/4);
+        }
+
+        private Vector3 ScreenCenterToMouse()
+        {
+            return Input.mousePosition - ScreenMiddle();
+        }
+
+        private Vector3 ScreenMiddle()
+        {
+            _currentResolution = Screen.currentResolution;
+            return new Vector3(_currentResolution.width / 2f, _currentResolution.height / 2f, 0);
+        }
+    }
+}
