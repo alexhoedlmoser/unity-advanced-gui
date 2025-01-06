ab82c6aad464031f3428e586178852eaad1b9cf0
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2025-01-06 01:12:14 +0100

    Disable GameObjectMenu creation with prefabs

--- Editor/GameObjectMenu.cs
+++ Editor/GameObjectMenu.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -60,9 +61,7 @@ namespace AlexH.AdvancedGUI.Editor
               
         }
         
-        
-        
-        
     }
 }
+*/
 
